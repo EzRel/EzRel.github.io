e77fe506e69465b50afdc30678d4b994b8aa2cd0
EzRel <35404657+EzRel@users.noreply.github.com>
2018-01-22 20:24:46 +0200

    v1.1.7
    FUNREWRITE! SearchInside is GONE!
    Added rewapp and htaccess. Updated some code.

---
+++ funrewrite/rewapp.js
@@ -0,0 +1,16 @@
+var appBody = 'Bookmarks can change code ;)';
+
+function searchInside(x) {
+	var y = 'DONE!';
+	document.getElementsByTagName('body')[0].innerHTML = document.getElementsByTagName('body')[0].innerHTML + y;
+	appBody = x + y;
+}
+
+function spaceEmoji() {
+	document.getElementsByTagName('body')[0].innerHTML = appBody.replace(" ", maChoose(["&#128512;", "&#128513;", "&#128514;", "&#128515;", "&#128516;", "&#128517;", "&#128518;"]));
+}
+
+function maChoose(choices) {
+	var index = Math.floor(Math.random() * choices.length);
+	return choices[index];
+}
--- searchinside/srcins.js
+++
@@ -1,5 +0,0 @@
-function searchInside() {
-	document.getElementsByTagName('body')[0].innerHTML = document.getElementsByTagName('body')[0].innerHTML+'<br><br><br><br>IN PROGRESS<br><br>As of January 23rd of 2018, SearchInside was launched publicly but is still in BETA phase.'
-}
-
-searchInside();
