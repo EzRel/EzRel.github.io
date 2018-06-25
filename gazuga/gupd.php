d6758c1937072f48e891ce51f1e667b0c7b70e48
EzRel <35404657+EzRel@users.noreply.github.com>
2018-06-25 09:49:19 +0300

    g1.051

--- gazuga/gupd.php
+++ gazuga/gupd.php
@@ -1,6 +1,24 @@
 <?php
 
+	switch ($_SERVER['HTTP_ORIGIN']) {
+		case 'http://from.com': case 'https://from.com':
+		header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
+		header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+		header('Access-Control-Max-Age: 1000');
+		header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+		header('Access-Control-Allow-Headers: x-csrf-token');
+		break;
+	}
+
 	$t = $_POST['t'];
+
+	if($t == "") {
+		$t = $_REQUEST['t'];
+	}
+
+	if($t == "") {
+		$t = "random";
+	}
 
 	$p = explode("~[~]~", file_get_contents($t));
 
